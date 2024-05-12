692007610de471eed92612940b5572a81ba44177
Yash Rawat <112065069+y3-rawat@users.noreply.github.com>
2024-05-12 18:43:23 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,14 @@
 from http.server import BaseHTTPRequestHandler
+from langchain_groq import ChatGroq
 
 class handler(BaseHTTPRequestHandler):
     def ge(self):
+        key = "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"
+        llm_groq = ChatGroq(groq_api_key=key, model_name="llama3-70b-8192")
+        final_title = llm_groq.invoke("hy there how are you").content
+        
         # Corrected method to include self and return a string
-        return "This is a sample response from ge method."
+        return final_title
 
     def do_GET(self):
         # Send HTTP status 200 (OK)
