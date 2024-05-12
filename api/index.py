f2686b0db6a9bc47962a615e7d17c9bc3ec6f73d
Yash Rawat <112065069+y3-rawat@users.noreply.github.com>
2024-05-12 18:32:35 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,12 @@
 from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
-
+    def ge():
+        return "asd"
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
+        g()
         self.wfile.write('Hello, world!'.encode('utf-8'))
         return
